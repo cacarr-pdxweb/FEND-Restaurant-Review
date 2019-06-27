a0b24df25e746f476a5dc41c97194a2356f1c015
cacarr-pdxweb <christopher.carr@pdxwebguy.com>
2019-06-27 05:15:03 -0700

    Cleaned up SW.js comments

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ const urlsToCache = [
     'img/10.jpg'
 ];
 
-//// Application shell is cached and pages display offline with only install event - meets rubric
+//// Application shell is cached and pages display offline 
 
 const projectCache = "project-cache-v1";
 
@@ -45,69 +45,68 @@ self.addEventListener('install', (e) => {
 
         // Opens the cache 
         caches.open(projectCache)
-        .then((cache) => {
+            .then((cache) => {
 
-            // Add files to cache - cache application shell 
-            console.log('[ServiceWorker] Adding files in urlsToCache array to cache');
-            return cache.addAll(urlsToCache);
-        
-        // Promise resolved
-        })
+                // Add files to cache - cache application shell 
+                console.log('[ServiceWorker] Adding files in urlsToCache array to cache');
+                return cache.addAll(urlsToCache);
+
+                // Promise resolved
+            })
     );
 });
 
 
-
-/** === TODO: Debug Broken Fetch event - add activate event to handle outdated caches === **/
+/** === TODO: add activate event to handle outdated caches === **/
 
 // Fetch 
 self.addEventListener('fetch', (e) => {
 
     console.log('[ServiceWorker] Fetch event for ', e.request);
-    
+
     // Fetch event response
     e.respondWith(
-        
+
         // Check cache for request
         caches.match(e.request)
-        
-        .then((response) => {
-            
-            // If request in cache
-            if (response) {
-                
-                console.log('[ServiceWorker] Found', e.request.url, ' in cache');
-                
-                // Return cached version
-                return response;
-            }
-            console.log('[ServiceWorker] Network request for ', e.request.url);
-            return fetch(e.request)
 
-            
-            // If request not in cache, fetch and cache
             .then((response) => {
 
-                // Open cache 
-                return caches.open(projectCache)
-                
-                .then((cache) => {
-                    
-                    // Put fetched response in cache
-                    cache.put(e.request.url, respononse.clone());
-                    
-                    // Return response
+                // If request in cache
+                if (response) {
+
+                    console.log('[ServiceWorker] Found', e.request.url, ' in cache');
+
+                    // Return cached version
                     return response;
-                });
-            });
-            
-            
+                }
+                console.log('[ServiceWorker] Network request for ', e.request.url);
+                return fetch(e.request)
 
-        }).catch((error) => {
 
-            // TODO: Consider adding offline page
+                    // If request not in cache, fetch and cache
+                    .then((response) => {
 
-        })
+                        // Open cache 
+                        return caches.open(projectCache)
+
+                            .then((cache) => {
+
+                                // Put fetched response in cache
+                                cache.put(e.request.url, respononse.clone());
+
+                                // Return response
+                                return response;
+                            });
+                    });
+
+
+
+            }).catch((error) => {
+
+                // TODO: Consider adding offline page
+
+            })
     );
 }); 
 
